01314b4a3faeefb963e00f5471e6a3386be5cdfb
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-05-02 23:26:51 -0700

    Add initial part of computer_guessing game algorithm. 3 starting guesses

--- mastermind.rb
+++ mastermind.rb
@@ -5,6 +5,68 @@ class Game
     end
 
     def play_game
+        
+    end
+
+
+
+    def play_computer_guessing_game
+        puts "Input a random 4-digit code for the computer to guess. Each digit is between 1-6"
+
+        # while true
+        #     puts "Enter code :"
+
+        #     code_string = gets.chomp
+
+        #     # Check for 4 digits. Does not check if entry is an integer
+        #     if code_string.length == 4
+        #         @board.user_defined_code(code_string)
+        #         puts "Code #{code_string} stored"
+        #         break
+        #     end
+        # end
+        @board.user_defined_code("1234")
+
+        # start with three guesses: 1122, 3344, 5566
+        starting_guesses = []
+        starting_guesses.push(Guess.new("1122"))
+        starting_guesses.push(Guess.new("3344"))
+        starting_guesses.push(Guess.new("5566"))
+
+        starting_guesses.each do |guess_object|
+            correct_match = @board.check_guess(guess_object)
+            @board.store_guess(guess_object)
+            @board.render_board
+
+            # For the off chance that the starting guesses are the correct code OR if there are too many starting guesses
+            if correct_match
+                puts "Computer guessed the code: #{@board.correct_code.join}"
+                break
+            elsif @board.number_of_guesses >= 12
+                puts "Computer ran out of guesses"
+                break
+            end
+        end
+
+        p @board.guess_history
+
+
+
+
+
+        # look in feedback for all guesses
+        # hard brackets [] -> number and position stored in known_code_array
+        # soft brackets () -> add to a hash similar to the array#tally output. Try these numbers in the known frequency in the unknown positions
+        
+
+
+    end
+
+    def play_human_guessing_game
+        
+        # Generate a random code for the human to guess
+        @board.set_random_code
+
         puts "Guess a random 4-digit code. Each digit is between 1-6"
  
         while true
@@ -18,10 +80,10 @@ class Game
                 next
             end
 
-            guess = Guess.new(guess_string)
+            guess_object = Guess.new(guess_string)
 
-            correct_match = @board.check_guess(guess)
-            @board.store_guess(guess)
+            correct_match = @board.check_guess(guess_object)
+            @board.store_guess(guess_object)
             @board.render_board
 
             if correct_match
@@ -37,31 +99,41 @@ end
 
 class Board
     attr_reader :correct_code
+    attr_reader :guess_history
+    attr_reader :feedback_history
 
     def initialize
-        @correct_code = generate_random_code
-        # @correct_code = [1,1,2,2]
-        @guesses = []
+        @correct_code = []
+        @guess_history = []
+        @feedback_history = [] # delete this if using @guess_history works out instead
     end
 
     # generate 4-digit random number with each digit between 1-6
-    def generate_random_code
+    def set_random_code
         random_code = []
         
         4.times do
             random_code.push(rand(1..6))
         end
 
-        random_code
+        @correct_code = random_code
+    end
+
+    def user_defined_code(code_string)
+        # need to convert input to string, split into array of chars, then convert each element back to integer
+        code_array = code_string.split("")
+        code_array = code_array.map {|string| string = string.to_i}
+
+        @correct_code = code_array
     end
 
     # return the number of guesses made
     def number_of_guesses
-        @guesses.length
+        @guess_history.length
     end
 
-    def check_guess(guess)
-        guess_array = guess.guess_array
+    def check_guess(guess_object)
+        guess_array = guess_object.guess_array
         feedback_array = Array.new(4) {nil}
         unmatched_code_tally = @correct_code.tally # critical for pass #2, so that it ignores pass #1 matches
         
@@ -91,7 +163,8 @@ class Board
             end
         end
 
-        guess.store_feedback(feedback_array)
+        guess_object.store_feedback(feedback_array)
+        @feedback_history.push(feedback_array)
 
         # return true if a complete match is found
         if feedback_array == ["[]", "[]", "[]", "[]"] then true else false end
@@ -99,7 +172,7 @@ class Board
 
     # store a guess in the history of guesses
     def store_guess(guess)
-        @guesses.push(guess)
+        @guess_history.push(guess)
     end
 
     # Display the history of guesses, feedback, legend and num guesses remaining
@@ -107,8 +180,8 @@ class Board
         puts ""
         puts "Guesses: "
 
-        for i in 0...@guesses.length do
-            puts "#{i+1}: #{@guesses[i].to_s}" #unsure why I need to write to_s, shouldnt this access the to_s method by default?
+        for i in 0...@guess_history.length do
+            puts "#{i+1}: #{@guess_history[i].to_s}" #unsure why I need to write to_s, shouldnt this access the to_s method by default?
         end
         puts ""
         puts "Legend: "
@@ -163,7 +236,7 @@ class Guess
 end
 
  a_game = Game.new
- a_game.play_game
+ a_game.play_computer_guessing_game
 
 
 # guess = Guess.new("1234")
