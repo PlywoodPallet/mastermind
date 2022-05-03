b21cffc64b749040345a9ccd6753a3c47bf7fa0b
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-05-03 14:21:41 -0700

    Add play_computer_guessing_game. Found a nasty bug when given a random code to guess. @Board#check_guess is not giving proper feedback

--- mastermind.rb
+++ mastermind.rb
@@ -25,7 +25,10 @@ class Game
         #         break
         #     end
         # end
-        @board.user_defined_code("1234")
+        # @board.user_defined_code("1234")
+
+        @board.set_random_code
+        p "Random code: #{@board.correct_code}"
 
         # start with three guesses: 1122, 3344, 5566
         starting_guesses = []
@@ -48,15 +51,81 @@ class Game
             end
         end
 
-        p @board.guess_history
-
-
-
-
+        
+        # p @board.guess_history
 
         # look in feedback for all guesses
         # hard brackets [] -> number and position stored in known_code_array
-        # soft brackets () -> add to a hash similar to the array#tally output. Try these numbers in the known frequency in the unknown positions
+        # soft brackets () -> add number to a probable_code_options array
+
+        known_code_array = Array.new(4) {nil}
+        nil_indexes = []
+        probable_code_options = []
+        guess_history = @board.guess_history
+
+        guess_history.each do |guess|
+            guess_array = guess.guess_array
+            feedback_array = guess.feedback_array
+
+            for i in 0...guess_array.length
+                if feedback_array[i] == nil
+                    next
+                elsif feedback_array[i] == "[]"
+                    known_code_array[i] = guess_array[i]
+                elsif feedback_array[i] == "()"
+                    probable_code_options.push(guess_array[i])
+                end
+            end
+        end
+
+
+         
+
+
+        # keep guessing using probable_code_options elements until a complete match or run out of guesses
+        # nil indexes = index of each nil item in known_code_array
+        nil_indexes = known_code_array.each_index.select { |index| known_code_array[index] == nil}
+
+        while true
+
+            # create a new guess based on known_code_array with "nil" entries replaced with random elements in probable_code_options
+            temp_probable_code_options = probable_code_options.clone
+            new_guess_array = known_code_array.clone
+
+            # for each nil index, pick a probable code at random to guess at that specific index
+            nil_indexes.each do |nil_index|
+                # pick a random digit then delete from the temp array so it cannot be chosen again
+                random_code_digit = temp_probable_code_options.sample
+                # temp_probable_code_options.delete_at(temp_probable_code_options.index(random_code_digit))
+
+                new_guess_array[nil_index] = random_code_digit
+            end
+
+            new_guess_string = new_guess_array.join
+            new_guess_object = Guess.new(new_guess_string)
+
+
+            # check if the new guess has already been checked. If so, generate a new guess
+            matching_incorrect_guesses = @board.guess_history.select {|incorrect_guess| incorrect_guess.guess_array == new_guess_object.guess_array}
+            unless matching_incorrect_guesses == []
+                next
+            end
+            
+            correct_match = @board.check_guess(new_guess_object)
+            @board.store_guess(new_guess_object)
+            @board.render_board
+
+            if correct_match
+                puts "Computer guessed the code: #{@board.correct_code.join}"
+                break
+            elsif @board.number_of_guesses >= 12
+                puts "Computer ran out of guesses"
+                break
+            else
+                # incorrect_guess_history.push(new_guess_object)
+            end
+        end
+        
         
 
 
