482f1de0a4f229770fd9e6b3875f87dddcfbf441
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-05-02 18:09:34 -0700

    Working version where human guesses random code. Board displays feedback for two different cases. A correct guess and too many guesses ends the game

--- mastermind.rb
+++ mastermind.rb
@@ -8,13 +8,11 @@ class Game
         puts "Guess a random 4-digit code. Each digit is between 1-6"
  
         while true
-            
-            
             puts "Enter guess:"
 
             guess_string = gets.chomp
 
-            #check for correct input (4 digit integer only)
+            # Check for 4 digits. Does not check if entry is an integer
             unless guess_string.length == 4
                 puts "Incorrect number of digits"
                 next
@@ -22,19 +20,27 @@ class Game
 
             guess = Guess.new(guess_string)
 
-
-    
-            @board.check_guess(guess)
+            correct_match = @board.check_guess(guess)
             @board.store_guess(guess)
             @board.render_board
+
+            if correct_match
+                puts "Congratulations, you guessed the code: #{@board.correct_code.join}"
+                break
+            elsif @board.number_of_guesses >= 12
+                puts "Rats! You ran out of guesses"
+                break
+            end
         end
     end
 end
 
 class Board
+    attr_reader :correct_code
+
     def initialize
-        #@correct_code = generate_random_code
-        @correct_code = [1,2,3,4]
+        @correct_code = generate_random_code
+        # @correct_code = [1,1,2,2]
         @guesses = []
     end
 
@@ -49,42 +55,54 @@ class Board
         random_code
     end
 
+    # return the number of guesses made
+    def number_of_guesses
+        @guesses.length
+    end
 
     def check_guess(guess)
         guess_array = guess.guess_array
         feedback_array = Array.new(4) {nil}
-        remaining_unchecked_code = Array.new(4) {nil}
+        unmatched_code_tally = @correct_code.tally # critical for pass #2, so that it ignores pass #1 matches
         
         # Pass #1: check for correct code AND correct position
         for i in 0...4
             if @correct_code[i] == guess_array[i]
                 # puts "correct digit at position #{i+1}" #user readable position (1-4)
                 feedback_array[i] = "[]"
-            else
-                remaining_unchecked_code[i] = guess_array[i]
+
+                # remove one from the unmatched code tally to account for match
+                unmatched_code_tally[guess_array[i]] -= 1
             end
         end
 
-        # # Pass #2: check for correct code AND wrong position
-        # # ignore digits with correct code and position (Ignore matches from Pass #1)
-        # for i in 0...4
-        #     # skip digit in guess_array if digit is already correct AND correct position
-        #     if feedback_array[i] == "[]"
-        #         next
-        #     elsif remaining_unchecked_code.any? {guess_array[i]}
-        #         puts "digit at position #{i+1} exists but is in a wrong position"
-        #         feedback_array[i] = "()"
+        # Pass #2: check for correct code AND wrong position from unchecked part of the code only
+        for i in 0...4
+            tally_result = unmatched_code_tally[guess_array[i]]
+            
+            # perform this check first to prevent a nil > 0 comparison below 
+            if tally_result == nil
+                break
+            elsif tally_result > 0
+                feedback_array[i] = "()"
 
-        # end
+                # remove one from the unmatched code tally to account for match
+                unmatched_code_tally[guess_array[i]] -= 1
+            end
+        end
 
         guess.store_feedback(feedback_array)
+
+        # return true if a complete match is found
+        if feedback_array == ["[]", "[]", "[]", "[]"] then true else false end
     end
 
+    # store a guess in the history of guesses
     def store_guess(guess)
         @guesses.push(guess)
     end
 
-    # Number of guesses, feedback by 
+    # Display the history of guesses, feedback, legend and num guesses remaining
     def render_board
         puts ""
         puts "Guesses: "
@@ -96,6 +114,8 @@ class Board
         puts "Legend: "
         puts "[] = correct digit and position"
         puts "() = digit exists but incorrect position"
+        puts ""
+        puts "#{12-number_of_guesses} guesses remaining"
         puts ""
     end
 end
@@ -123,7 +143,7 @@ class Guess
         @feedback_array = feedback_array
     end
 
-    # Important function so Board#render_board works. @guess_array and @feedback_array are combined a simple way to read the result of a guess
+    # Important function so Board#render_board works. @guess_array and @feedback_array are combined a simple way to display the result of a guess
     def to_s
         output_array = []
 
@@ -134,10 +154,10 @@ class Guess
                 output = feedback_prefix + guess_array[i].to_s + feedback_postfix
                 output_array.push(output)
             else
-                output_array.push(guess_array[i])
+                output_array.push(" #{guess_array[i]} ")
             end
         end
-        output_array.join(', ')
+        output_array.join(' ')
     end
 
 end
